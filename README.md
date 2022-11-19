64522f65dd7bd94d0ea4fad08efc5ec44649af36
andreijsecor <andreijsecor@gmail.com>
2022-11-19 11:08:03 -0800

    finished part 2

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Lab 8 - Starter
 1. I would automatically run unit tests using github actions (or equivalent) whenever code is pushed. This allows the tests to be easily automated and runs them regularly to make the debugging process easier.
 2. Generally, end-to-end tests are not a good choice when testing the output of a single function because users will not interact with a product that thoroughly due to the UI limitations.
-3. 
+3. A unit test would generally not be used for this purpose, since it involves at least two separate modules (sending a message + receiving a message on another client).
+4. In this case, it appears that the max length feature is part of the message typing feature, which makes these two features part of the same module. As such, a unit test would be appropriate.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,93 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+//isPhoneNumber()
+
+test('makes sure 510-555-5555 is valid', () => {
+    expect(functions.isPhoneNumber('510-555-5555')).toBe(true);
+})
+
+test('makes sure (666)-6666-6666 is valid', () => {
+    expect(functions.isPhoneNumber('(666)-6666-6666')).toBe(true);
+})
+
+test('makes sure 123-456-789-10 is invalid', () => {
+    expect(functions.isPhoneNumber('123-456-789-10')).toBe(false);
+})
+
+test('makes sure empty string is invalid', () => {
+    expect(functions.isPhoneNumber('')).toBe(false);
+})
+
+//isEmail()
+
+test('makes sure abc@xyz.com is valid', () => {
+    expect(functions.isEmail('abc@xyz.com')).toBe(true);
+})
+
+test('makes sure abracadabra@magico.uk is valid', () => {
+    expect(functions.isEmail('abracadabra@magico.uk')).toBe(true);
+})
+
+test('makes sure banana@tree is invalid', () => {
+    expect(functions.isEmail('banana@tree')).toBe(false);
+})
+
+test('makes sure empty string is invalid', () => {
+    expect(functions.isEmail('')).toBe(false);
+})
+
+//isStrongPassword()
+
+test('makes sure Iamapassword is valid', () => {
+    expect(functions.isStrongPassword('Iamapassword')).toBe(true);
+})
+
+test('makes sure A123a456A789 is valid', () => {
+    expect(functions.isStrongPassword('A123a456A789')).toBe(true);
+})
+
+test('makes sure Iamabittoolong__ is invalid', () => {
+    expect(functions.isStrongPassword('Iamabittoolong__')).toBe(false);
+})
+
+test('makes sure noFaces:( is invalid', () => {
+    expect(functions.isStrongPassword('noFaces:(')).toBe(false);
+})
+
+//isDate()
+
+test('makes sure 1/1/1001 is valid', () => {
+    expect(functions.isDate('1/1/1001')).toBe(true);
+})
+
+test('makes sure 22/11/2022 is valid', () => {
+    expect(functions.isDate('22/11/2022')).toBe(true);
+})
+
+test('makes sure 10/10/100O is invalid', () => {
+    expect(functions.isDate('10/10/100O')).toBe(false);
+})
+
+test('makes sure empty string is invalid', () => {
+    expect(functions.isDate('')).toBe(false);
+})
+
+//isHexColor()
+
+test('makes sure #0FF00F is valid', () => {
+    expect(functions.isHexColor('#0FF00F')).toBe(true);
+})
+
+test('makes sure abc is valid', () => {
+    expect(functions.isHexColor('abc')).toBe(true);
+})
+
+test('makes sure abdefg is invalid', () => {
+    expect(functions.isHexColor('abdefg')).toBe(false);
+})
+
+test('makes sure #beef is invalid', () => {
+    expect(functions.isHexColor('#beef')).toBe(false);
+})
